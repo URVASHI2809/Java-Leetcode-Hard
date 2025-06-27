c6bdc7a65c8460731de8461732b8a6a7caae06f5
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2025-06-27 19:11:16 +0530

    LEETCODE -> 2014. Longest Subsequence Repeated k Times

---
+++ LongestSubsequenceRepeatedK.java
@@ -0,0 +1,42 @@
+class LongestSubsequenceRepeatedK {
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        char[] ca= s.toCharArray();
+        int n= ca.length;
+        
+        char[] freq= new char[26];
+        for(int i=0; i<n; i++)
+            ++freq[ca[i]-'a'];
+
+        ArrayList<String>[] cand= new ArrayList[8];
+        cand[1]= new ArrayList<>();
+        String ans= "";
+        for(int i=0; i<26; i++)
+            if(freq[i]>=k)
+                cand[1].add(ans= "" + (char)('a'+i));
+        
+        for(int i=2; i<8; i++){
+            cand[i]= new ArrayList<>();
+            for(String prev:cand[i-1])
+                for(String c:cand[1]){
+                    String next= prev+c;
+                    if(isSubsequenceRepeatedK(ca, next, k))
+                        cand[i].add(ans= next);
+                }
+        }
+        return ans;
+    }
+    
+    boolean isSubsequenceRepeatedK(char[] ca, String t, int k){
+        char[] ta= t.toCharArray();
+        int n= ca.length, m= ta.length, i= 0;
+        while(k-->0){
+            int j=0;
+            while(i<n && j<m){
+                if(ca[i]==ta[j]) j++;
+                i++;
+            }
+            if(j!=m) return false;
+        }
+        return true;
+    }
+}
